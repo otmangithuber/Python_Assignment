441333cebf799d870900bbc5a92981983593a726
Otman Addou Kamra <125973845+otmangithuber@users.noreply.github.com>
2023-06-03 18:27:21 +0100

    Add files via upload

---
+++ Python assignment M2C3.py
@@ -0,0 +1,47 @@
+# EXERCISE1
+
+string = "coding is not difficult"
+number = 9
+list = ['first', 'second', 'third']
+boolean = True
+
+# EXERCISE2
+
+index = string[:3]
+print(index)
+
+# EXERCISE3
+
+first_element = list[0]
+
+print(first_element)
+
+# EXERCISE4
+
+num = number + 10
+print(num)
+
+# EXERCISE5
+
+last_element = list[-1]
+print(last_element)
+
+# EXERCISE6
+
+names = 'harry,alex,susie,jared,gail,conner'
+list_of_names = names.split()
+print(list_of_names)
+
+# EXERCISE7
+
+word = 1
+first_word = string.split(' ')[word-1]
+print(first_word.upper())
+
+# EXERCISE8
+
+interpolation = f"I have just finished {number} exercises of Python."
+print(interpolation)
+
+# EXERCISE9
+print("hello world")
